ab277036ebc5ab69e88c3059582051b61975190a
shantimoktan <shantimoktan663@gmail.com>
2022-10-20 00:20:09 +0545

    feat: open context menu to left if it overflows then window width

--- main.js
+++ main.js
@@ -3,10 +3,14 @@
 const menuel = document.querySelector(".menu-list");
 document.addEventListener('contextmenu', event => {
     event.preventDefault();
-    if(event.target.className !== 'menu-item'){
-        menuel.style.top = event.pageY+"px";
-        menuel.style.left = event.pageX+"px";
-        menuel.style.display="block";
+    if (event.target.className !== 'menu-item') {
+        if (event.pageX >= (window.innerWidth - 250)) {
+            menuel.style.left = (event.pageX - 250) + "px";
+        } else {
+            menuel.style.left = event.pageX + "px";
+        }
+        menuel.style.top = event.pageY + "px";
+        menuel.style.display = "block";
     }
 });
 
